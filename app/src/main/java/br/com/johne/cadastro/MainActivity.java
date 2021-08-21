f8ed82841b0a75a7679a3708cf37351bdd65bec3
johnepuerta <64877717+johnepuerta@users.noreply.github.com>
2021-08-21 01:39:57 -0300

    final version

--- app/src/main/java/br/com/johne/cadastro/MainActivity.java
+++ app/src/main/java/br/com/johne/cadastro/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,10 +18,13 @@ public class MainActivity extends AppCompatActivity {
     private EditText phoneEd;
     private EditText mailEd;
     private CheckBox mailCb;
+    private RadioGroup radioGroup;
     private RadioButton genderMaleRb;
     private RadioButton genderFemaleRb;
     private EditText cityEd;
     private Spinner statesSp;
+    private Button saveBt;
+    private Button clearBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +35,36 @@ public class MainActivity extends AppCompatActivity {
         phoneEd = findViewById(R.id.phoneEd);
         mailEd = findViewById(R.id.mailEd);
         mailCb = findViewById(R.id.mailCb);
+        radioGroup = findViewById(R.id.radioGroup);
         genderMaleRb = findViewById(R.id.genderMaleRb);
         genderFemaleRb = findViewById(R.id.genderFemaleRb);
         cityEd = findViewById(R.id.cityEd);
         statesSp = findViewById(R.id.statesSp);
 
-        Button saveBt = findViewById(R.id.saveBt);
-        saveBt.setOnClickListener(view -> {
+        saveBt = findViewById(R.id.saveBt);
+        clearBt = findViewById(R.id.cleanBt);
+
+        saveBt.setOnClickListener(click -> {
             Formulario form = new Formulario(nameEd.getText().toString(),
                     phoneEd.getText().toString(),
                     mailEd.getText().toString(),
-                    true,
+                    mailCb.isChecked(),
                     genderMaleRb.isChecked() ? genderMaleRb.getText().toString() : genderFemaleRb.getText().toString(),
                     cityEd.getText().toString(),
                     ((TextView) statesSp.getSelectedView()).getText().toString() );
 
             Toast.makeText(getApplicationContext(), form.toString(), Toast.LENGTH_LONG).show();
+        });
+
+        clearBt.setOnClickListener(click -> {
+            nameEd.setText("");
+            phoneEd.setText("");
+            mailEd.setText("");
+            mailCb.setChecked(false);
+            radioGroup.check(genderMaleRb.getId());
+            cityEd.setText("");
+            statesSp.setSelection(0);
+            nameEd.requestFocus();
         });
     }
 
